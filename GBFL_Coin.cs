e731004f036e326cd980dcb3282ba0853b891608
ConanODoyle <this.is.not.a.email.account@gmail.com>
2017-07-15 22:32:35 -0700

    minor bugfixes to coin

--- GBFL_Coin.cs
+++ GBFL_Coin.cs
@@ -151,12 +151,16 @@ function GBFLCoinImage::onDropCoin(%this, %obj, %slot) {
 	%u = mACos(vectorDot("1 0 0", %trans)) * -1;
 
 	%i.setTransform(vectorAdd(%obj.getHackPosition(), vectorScale(%obj.getForwardVector(), 2)) SPC %xyz SPC %u);
+	%i.setVelocity(vectorAdd(%obj.getVelocity(), vectorScale(%obj.getForwardVector(), 5)));
 	MissionCleanup.add(%i);
 	%i.hideNode("ALL");
 	schedule(50, %i, setCoinNodes, %i);
 
 	%heads = getRandom();
-	if (%heads < 0.5) {
+	if ($alt) {
+		%i.playThread(0, flip_side);
+		schedule(3000, 0, serverPlay2D, $impact);
+	} else if (%heads < 0.5) {
 		%i.playThread(0, flip_heads);
 		schedule(3000, 0, serverPlay2D, $impact);
 	} else {
@@ -177,11 +181,16 @@ function popCoin(%i) {
 		datablock = spawnProjectile;
 		initialPosition = %i.getPosition();
 	};
+	%p.setScale("1.5 1.5 1.5");
 	%p.explode();
 	%i.delete();
 }
 
 function setCoinNodes(%i) {
+	if ($alt) {
+		setCoinNode(%i);
+		return;
+	}
 	serverPlay2D(BlankABallFiresound);
 	%i.hideNode("ALL");
 	%i.unHideNode("coinG");
@@ -204,4 +213,63 @@ function setCoinNodes(%i) {
 	%i.setNodeColor("gbflG", %grey);
 	%i.setNodeColor("gbflD", %dark);
 	%i.setNodeColor("gbflW", %white);
+}
+
+function setCoinNode(%i) {
+	serverPlay2D(BlankABallFiresound);
+	%i.hideNode("ALL");
+	%i.unHideNode("coinG");
+	%i.unHideNode("coinW");
+	%i.unHideNode("coinN");
+	// %i.unHideNode("gbflG");
+	// %i.unHideNode("gbflD");
+	// %i.unHideNode("gbflW");
+	%i.unHideNode("yD");
+	%i.unHideNode("yW");
+
+	%white = (247/255) SPC (235/255) SPC 0 SPC "1";
+	%gold = (255/255) SPC (212/255) SPC 0 SPC "1";
+	%grey = (255/255) SPC (195/255) SPC 0 SPC "1";
+	%dark = (234/255) SPC (168/255) SPC 0 SPC "1";
+
+	%i.setNodeColor("coinG", %grey);
+	%i.setNodeColor("coinW", %white);
+	%i.setNodeColor("coinN", %gold);
+	%i.setNodeColor("yD", %dark);
+	%i.darkColor = %dark;
+	%i.setNodeColor("yW", %white);
+	%i.whiteColor = %dark;
+
+	schedule(5000, %i, colorLoop, %i);
+}
+
+function colorLoop(%i) {
+	cancel (%i.colorLoop);
+	%dC = %i.darkColor;
+	%wC = %i.whiteColor;
+	if (%dc !$= "0 0 0 1") {
+		%a = getWord(%dc, 0) / 1.1;
+		if (%a < 0.05) {
+			%a = 0;
+		}
+		%i.darkColor = %a SPC %a SPC %a SPC 1;
+		%i.setNodeColor("yD", %i.darkColor);
+		%ct++;
+	}
+
+	if (%wc !$= "1 1 1 1") {
+		%a = (1 - getWord(%wc, 0)) / 1.1;
+		if (%a < 0.05) {
+			%a = 0;
+		}
+		%a = 1 - %a;
+		%i.whiteColor = %a SPC %a SPC %a SPC 1;
+		%i.setNodeColor("yW", %i.whiteColor);
+		%ct++;
+	}
+
+	if (%ct $= "") {
+		return;
+	}
+	%i.colorLoop = schedule(100, %i, colorLoop, %i);
 }
